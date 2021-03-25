ee93fc45c498a1cc6daa6b53710200ab9f485370
bygorton <hec20tor0@gmail.com>
2021-03-25 20:11:15 +0100

    Defensa

--- src/Defensa.java
+++ src/Defensa.java
@@ -1,2 +1,13 @@
-public class Defensa {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.Reader;
+
+public abstract class Defensa {
+    public String tipoDeDefensa() throws IOException {
+        Reader i = new InputStreamReader(System.in);
+        BufferedReader br = new BufferedReader(i);
+        String s = br.readLine();
+        return s;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
-public class Main{
-        public static void main (String [ ] args) {
-            System.out.println ("Empezamos la ejecución del programa");
-        }
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Empezamos la ejecución del programa");
+    }
 }
