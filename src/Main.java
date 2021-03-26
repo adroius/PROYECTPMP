fcd2aec98a2c4d6f476b0a54f870a3003ba2b365
adrian <adrianpenaga@gmail.com>
2021-03-26 20:30:17 +0100

    dia 26/03 ale a dormir

--- src/Main.java
+++ src/Main.java
@@ -4,12 +4,12 @@ public class Main {
     public static void main(String[] args) {
         Sistema sistema = new Sistema();
         sistema.getSistema();
-        boolean finalizar = false;
+        boolean finalizar;
         Scanner sc = new Scanner(System.in);
 
 
         Caza c=new Caza();
-        System.out.println(c.toString());
+
 
         do {
             System.out.println("¿Desea continuar?");
@@ -17,12 +17,9 @@ public class Main {
             System.out.println("2) NO");
             int s = sc.nextInt();
             switch (s) {
-                case 1:
-                    finalizar = false;
-                    break;
-                case 2:
-                    finalizar = true;
-                    break;
+                case 1 -> finalizar = false;
+                case 2 -> finalizar = true;
+                default -> throw new IllegalStateException("Unexpected value: " + s);
             }
         } while (!finalizar);
     }
