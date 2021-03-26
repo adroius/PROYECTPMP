d3c7371f59a38f5de140814376a3abed0b39b577
bygorton <hec20tor0@gmail.com>
2021-03-26 19:01:29 +0100

    Destructor

--- src/Destructor.java
+++ src/Destructor.java
@@ -1,24 +1,29 @@
 import java.util.Scanner;
 
 public class Destructor extends TipoDeNave{
-        int tripulantesMax=0;
-        int cargaMax=0;
-        String defensa="";
+    int tripulantesMax=0;
+    int cargaMax=0;
+    Defensa defensa;
 
-        public Destructor(){
-
-        }
-        public int sistemaDeDefensa(){
-
-            return  s;
-        }
-        public int carga(){
-            system.out.println("carga");
-            Scanner sc = new Scanner(System.in);
-            return (int c = sc.nextInt());
-        }
-        public string seleccionDefensa(){
-            defensa e = new defensa();
-            return d.toString();
-        }
+    public Destructor(){
+        this.tripulantesMax = tripulantes();
+        this.cargaMax = carga();
+        this.defensa= new Defensa();
+    }
+    public int tripulantes(){
+        System.out.println("¿?");
+        Scanner sc = new Scanner(System.in);
+        int s = sc.nextInt();
+        return  (s);
+    }
+    public int carga(){
+        System.out.println("carga");
+        Scanner sc = new Scanner(System.in);
+        int c = sc.nextInt();
+        return (c);
+    }
+    public String seleccionDefensa(){
+        Defensa e = new Defensa();
+        return e.toString();
+    }
 }
