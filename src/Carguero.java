900f1b479515263659a15a9dad0021b99d8922e5
JANI <s.buchelska@alumnos.urjc.es>
2021-03-26 18:19:32 +0100

    qwr

--- src/Carguero.java
+++ src/Carguero.java
@@ -1,2 +1,29 @@
 public class Carguero extends TipoDeNave{
+    int tripulantesMax=0;
+    int cargaMax=0;
+    String defensa="";
+
+    public Carguero(){
+        this.tripulantesMax = tripulantes();
+        this.cargaMax = carga();
+        this.defensa= seleccionDefensa();
+    }
+    public int tripulantes(){
+        System.out.println("¿?");
+        Scanner sc = new Scanner(System.in);
+        return  (int s = sc.nextInt());
+    }
+    public int carga(){
+        system.out.println("carga");
+        Scanner sc = new Scanner(System.in);
+        return (int c = sc.nextInt());
+    }
+    public string seleccionDefensa(){
+        defensa e = new defensa();
+        return d.toString();
+    }
+    @Override
+    public arrancar
+}
+
 }
--- src/Destructor.java
+++ src/Destructor.java
@@ -1,2 +1,25 @@
 public class Destructor extends TipoDeNave{
+        int tripulantesMax=0;
+        int cargaMax=0;
+        String defensa="";
+
+        public Carguero(){
+            this.tripulantesMax = tripulantes();
+            this.cargaMax = carga();
+            this.defensa= seleccionDefensa();
+        }
+        public int tripulantes(){
+            System.out.println("¿?");
+            Scanner sc = new Scanner(System.in);
+            return  (int s = sc.nextInt());
+        }
+        public int carga(){
+            system.out.println("carga");
+            Scanner sc = new Scanner(System.in);
+            return (int c = sc.nextInt());
+        }
+        public string seleccionDefensa(){
+            defensa e = new defensa();
+            return d.toString();
+        }
 }
