2a4fd3ec628f051f2bf3ba97824d11fc2493f648
adrian <adrianpenaga@gmail.com>
2021-03-29 11:33:46 +0200

    dia 29/03

--- src/Main.java
+++ src/Main.java
@@ -3,23 +3,6 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Sistema sistema = new Sistema();
-        sistema.getSistema();
-        boolean finalizar;
-        Scanner sc = new Scanner(System.in);
-        Caza c=new Caza();
-        Caza d=new Caza();
-        System.out.println(c);
-        System.out.println(d);
-        do {
-            System.out.println("¿Desea continuar?");
-            System.out.println("1) SI");
-            System.out.println("2) NO");
-            int s = sc.nextInt();
-            switch (s) {
-                case 1 -> finalizar = false;
-                case 2 -> finalizar = true;
-                default -> throw new IllegalStateException("Unexpected value: " + s);
-            }
-        } while (!finalizar);
+            sistema.getSistema();
     }
 }
--- src/Sistema.java
+++ src/Sistema.java
@@ -1,4 +1,33 @@
+import java.util.Scanner;
+
 public class Sistema {
+    public Sistema() {
+    }
+
     public void getSistema() {
+        boolean f;
+        do {
+            System.out.println("Bienvenido al concesionario espacial");
+            System.out.println("¿Que es lo que desea realizar?");
+            System.out.println("0) Ingresar cliente");
+            System.out.println("1) Registrarse nuevo cliente");
+            System.out.println("2) Ver ofertas");
+            System.out.println("3) Salir");
+            f=finalizar();
+        }while(!f);
+    }
+    public boolean finalizar(){
+        boolean finalizar;
+        Scanner sc = new Scanner(System.in);
+            System.out.println("¿Desea continuar?");
+            System.out.println("1) SI");
+            System.out.println("2) NO");
+            int s = sc.nextInt();
+            switch (s) {
+                case 1 -> finalizar = false;
+                case 2 -> finalizar = true;
+                default -> throw new IllegalStateException("Unexpected value: " + s);
+            }
+        return finalizar;
     }
 }
