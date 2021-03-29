cc524c365915444179c8ab0a43a3b70b3e989bcd
adrian <adrianpenaga@gmail.com>
2021-03-29 17:03:28 +0200

    dia 29/03

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,8 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        Nave n=NaveBuilder.CrearNave();
+        System.out.println(n.toString());
         Sistema sistema = new Sistema();
         sistema.getSistema();
     }
--- src/NaveBuilder.java
+++ src/NaveBuilder.java
@@ -1,35 +1,34 @@
 import java.util.Scanner;
 
-public class NaveBuilder extends Nave{
+public abstract class NaveBuilder extends Nave{
 
-    public NaveBuilder() {
-
-    }
-
-    public Nave seleccionarTipoDeNave(){
+    public static Nave CrearNave(){
         Nave n;
         System.out.println("Que nave desea construir:");
-        System.out.println("1)Caza");
-        System.out.println("2)Destructor");
-        System.out.println("3)Carguero");
-        System.out.println("4)Estacion espacial");
+        System.out.println("1) Caza");
+        System.out.println("2) Destructor");
+        System.out.println("3) Carguero");
+        System.out.println("4) Estacion espacial");
         Scanner sc = new Scanner(System.in);
         int s = sc.nextInt();
-        switch (s){
-            case 1:
-                n=new Caza();
-                break;
-            case 2:
-                n=new Destructor();
-                break;
-            case 3:
-                n=new Carguero();
-                break;
-            case 4:
-                n=new EstacionEspacial();
-                break;
-            default:
-                throw new IllegalStateException("Unexpected value: " + s);
+        switch (s) {
+            case 1 -> {
+                System.out.println("Ha seleccionada Caza");
+                n = new Caza();
+            }
+            case 2 -> {
+                System.out.println("Ha seleccionada Destructor");
+                n = new Destructor();
+            }
+            case 3 -> {
+                System.out.println("Ha seleccionada Carguero");
+                n = new Carguero();
+            }
+            case 4 -> {
+                System.out.println("Ha seleccionada Estacion Espacial");
+                n = new EstacionEspacial();
+            }
+            default -> throw new IllegalStateException("Unexpected value: " + s);
         }
         return n;
     }
