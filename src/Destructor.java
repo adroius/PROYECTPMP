d3c19395b4dc02e79db700c578e8d44019da7876
steyex <bysteyex@gmail.com>
2021-03-29 15:22:59 +0200

    prueba4

--- src/Destructor.java
+++ src/Destructor.java
@@ -1,42 +1,50 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Destructor extends TipoDeNave{
-    int tripulantesMax=0;
-    int cargaMax=0;
+    List<Arma> conjuntoDeArmas;
+    List<Defensa> conjuntoDefensa;
     Defensa defensa;
+    Propulsion prop;
 
     public Destructor(){
-        this.tripulantesMax = tripulantes();
-        this.cargaMax = carga();
-        this.defensa= tipoDeDefensa();
+        super();
+        this.prop= new Propulsion();
+        this.conjuntoDeArmas=conjuntoDeArmas();
     }
-    public int tripulantes(){
-        System.out.println("¿?");
-        Scanner sc = new Scanner(System.in);
-        int s = sc.nextInt();
-        return  (s);
+    public Destructor(int Defensa , Propulsion p){
+        super();
+        this.prop= p;
+        this.conjuntoDeArmas= conjuntoDeArmas();
     }
-    public int carga(){
-        System.out.println("carga");
-        Scanner sc = new Scanner(System.in);
-        int c = sc.nextInt();
-        return (c);
+    public Defensa getDefensa() {
+        return defensa;
     }
-    public Defensa tipoDeDefensa(){
-        System.out.println("Introduzca el tipo de defensa: ");
-        System.out.println("1) Escudo");
-        System.out.println("2) Blindaje");
+
+    @Override
+
+    public String toString() {
+        return "Destructor:" + "\nDefensa = " + conjuntoDefensa.toString() + "\nArmas: " +conjuntoDeArmas.toString()+ "\nPropulsion: "+prop.toString();
+    }
+
+    public List<Defensa> conjuntoDefensa(){
+        List<Defensa> armas = new ArrayList<>();
         Scanner sc = new Scanner(System.in);
+        System.out.println("¿Que defensas tiene?");
         int e = sc.nextInt();
-        switch (e) {
-            case 1:
-                Defensa d = new Escudo();
-                return d;
-            case 2:
-                d = new Blindaje();
-                return d;
-            default:
-                throw new IllegalStateException("Valor incorrecto: " + e);
+        while (e>2){
+            System.out.println("Solo puede tener dos defensas, ¿Cuantas posee?(1 o 2)");
+            e = sc.nextInt();
         }
+        do{
+            Defensa d=new Defensa();
+            Defensa.add(d);
+            e=e-1;
+        } while (e!=0);
+        return Defensa;
     }
+
+
+
 }
