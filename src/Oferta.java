ef9a9e8761b473f866e8d696af619b032187179c
pamonugi <pamonugi@gmail.com>
2021-03-30 16:13:14 +0200

    Patata

--- src/Oferta.java
+++ src/Oferta.java
@@ -1,8 +1,39 @@
+import java.util.List;
+import java.util.Date;
+
 public class Oferta{
     String nIdentificacion;
 
     public Oferta (String nIdentificacion){
         this.nIdentificacion = nIdentificacion;
+    }
+
+    public void getOferta () {
+
+    }
+
+    public List<Nave> ofertaNave (int ) {
+
+    }
+
+    public Date fechaLimite (int) {
+
+    }
+
+    public int PrecioOfertaTotal (int) {
+
+    }
+
+    public int ProteccionTotal (int) {
+
+    }
+
+    public int crearVotacion (int , int ) {
+
+    }
+
+    public String crearComentario (int , int ) {
+
     }
 
 
