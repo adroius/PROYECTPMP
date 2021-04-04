c6900c7bea4c769c5209ecab835977d79e4d7123
adrian <adrianpenaga@gmail.com>
2021-04-04 16:32:10 +0200

    dia 04/04

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,14 @@
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileWriter;
 
 //Nave n=NaveBuilder.CrearNave();
 public class Main {
-    public static void main(String[] args) {
-        new Sistema();
+    public static void main(String[] args){
+        try {
+            new Sistema();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
     }
 }
--- src/Sistema.java
+++ src/Sistema.java
@@ -1,20 +1,20 @@
-import java.io.FileWriter;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
 public class Sistema {
     List<Usuario> users = new ArrayList<>();
-
+    private int intentospermitidos = 3;
     //Constructor Sistema
-    public Sistema() {
+    public Sistema() throws FileNotFoundException {
         boolean f = false;
         Scanner sc = new Scanner(System.in);
         //Menu del sistema
         do {
             System.out.println("Bienvenido al concesionario espacial");
             System.out.println("1) Ingresar nuevo cliente");
-            System.out.println("2) Registrarse");
+            System.out.println("2) Iniciar Sesion");
             System.out.println("3) Salir");
             int s = sc.nextInt();
             switch (s) {
@@ -26,8 +26,8 @@ public class Sistema {
                 case 2 -> {
                     if (iniciarSesion()) {
                         menu();
-                        f = true;
                     }
+                    f = true;
                 }
                 case 3 -> f = true;
                 default -> throw new IllegalStateException("Unexpected value: " + s);
@@ -51,30 +51,55 @@ public class Sistema {
                 case 3 -> f = true;
                 default -> throw new IllegalStateException("Unexpected value: " + s);
             }
-        } while(!f);
+        } while (!f);
     }
 
     //Registrar Nuevo Cliente
     public Usuario registrarNuevoCliente() {
         Usuario u = new Usuario();
-        try
-        {
-            FileWriter escribir=new FileWriter("usercontraseña.txt",true);
+        try {
+            FileWriter escribir = new FileWriter("usercontraseña.txt", true);
             escribir.write(u.user);
             escribir.write(u.contraseña);
             escribir.write("\n");
             escribir.close();
-        }
-        catch(Exception e)
-        {
+        } catch (Exception e) {
             System.out.println("Error al escribir");
         }
         return u;
     }
 
     //Inciar Sesion
-    public boolean iniciarSesion() {
-        boolean ingresado = false;
-        return ingresado;
+    public boolean iniciarSesion() throws FileNotFoundException {
+        Scanner sc = new Scanner(System.in);
+        boolean encontrado = false;
+        System.out.println("Introduzca usuario");
+        String use = sc.next();
+        System.out.println("Introduzca contraseña");
+        use += sc.next();
+        try {
+            do {
+                BufferedReader br = new BufferedReader(new FileReader("usercontraseña.txt"));
+                String linea = "";
+                while ((linea = br.readLine()) != null) {
+                    if (linea.equalsIgnoreCase(use)) {
+                        encontrado = true;
+                        break;
+                    }
+                }
+                if (!encontrado) {
+                    intentospermitidos = intentospermitidos - 1;
+                    use=null;
+                    System.out.println("Datos erroneos");
+                    System.out.println("Introduzca usuario");
+                    use += sc.next();
+                    System.out.println("Introduzca contraseña");
+                    use += sc.next();
+                }
+            } while (use.equalsIgnoreCase("si") && intentospermitidos > 0);
+        } catch (IOException e) {
+            System.out.println("Error");
+        }
+        return encontrado;
     }
 }
