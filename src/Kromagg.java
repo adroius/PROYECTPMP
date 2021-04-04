641b07c4d3a944c2925c7dfc6592229ace373922
bygorton <hec20tor0@gmail.com>
2021-04-04 17:39:56 +0200

    Arma

---
+++ src/Kromagg.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class Kromagg extends Cliente{
+    boolean licencia;
+
+    private boolean licencia(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Tienes licencia?");
+        System.out.println("1) Si");
+        System.out.println("2) No");
+        int s = sc.nextInt();
+        switch (s) {
+            case 1: {
+                licencia = true;
+                break;
+            }
+            case 2: {
+                licencia = false;
+                break;
+            }
+        }
+        return licencia;
+    }
+    public Kromagg() {
+        if (licencia()){
+            NaveBuilder.CrearNave();
+        }
+        else {
+            NaveBuilder.CrearNaveEspecial();
+        }
+    }
+}
