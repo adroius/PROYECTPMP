8974bc1c4b152bbf3014ffd876eee2dae4b260f6
adrian <adrianpenaga@gmail.com>
2021-04-06 17:05:26 +0200

    dia 06/04

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,6 @@ import java.io.FileNotFoundException;
 //Nave n= NaveBuilder.CrearNave();
 public class Main {
     public static void main(String[] args) {
-        Nave n= NaveBuilder.CrearNave();
         try {
             new Sistema();
         } catch (FileNotFoundException e) {
