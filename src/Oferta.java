22c8a0da7b4155ed3d32ae57a651ccb713dce172
adrian <adrianpenaga@gmail.com>
2021-04-11 11:44:42 +0200

    dia 10/04

--- src/Oferta.java
+++ src/Oferta.java
@@ -1,54 +1,145 @@
 import java.io.*;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Date;
-import java.util.Scanner;
+import java.util.*;
 
-/*Clase Oferta
+//Clase Oferta
+
 public class Oferta {
     String nIdentificacion; //Numero de identificacion de Oferta
-    List<Oferta> Ofertas;
-
+    List<String> Ofertas;
 
     //Constructor Oferta
     public Oferta() {
-        this.Ofertas = menuOfertas();
-        this.nIdentificacion = nIdentificacion;
+        this.nIdentificacion = numaleatorios();
+    }
 
+    public String numaleatorios(){
+        int numero = (int)(Math.random()*10000+1000);
+        return String.valueOf(Math.abs(numero));
     }
 
     private void menuOfertas() {
 
     }
 
-    public void getOferta() {
-
+    public List<String> listaDeOfertas() {
+        List<String> lista = new ArrayList<>();
+        return lista;
     }
 
-    public List<Nave> ofertaNave(int c) {
+    public void construirOferta(String usuarioEntrar) throws IOException {
+        String usuarioAmeter = usuarioEntrar;
+        List<String> fichero = new ArrayList<>();
+        List<String> naves = new ArrayList<>();
+        BufferedReader br = new BufferedReader(new FileReader("userNaves.txt"));
+        String line;
+        boolean encontrado = false;
+        while ((line = br.readLine()) != null) {
+            fichero.add(line);
+        }
+        int min = 0;
+        int max = fichero.size() - 1;
+        String tope = "*";
+        if (max != 0 && Sistema.pertenece(usuarioAmeter)) {
+            while (!encontrado && max != 0) {
+                if (usuarioAmeter.equals(fichero.get(min))) {
+                    min = min + 1;
+                    while (!(fichero.get(min).equals(tope))) {
+                        naves.add(fichero.get(min));
+                        min = min + 1;
+                    }
+                    encontrado = true;
+                }
+                min = min + 1;
+                max = max - 1;
+            }
+        }
+        System.out.println("Que nave desea poner en venta:");
+        for (int i = 0; i < naves.size(); i++) {
+            if (naves.get(i).contains("Caza") || naves.get(i).contains("Carguero") || naves.get(i).contains("Destructor") || naves.get(i).contains("Estacion Espacial"))
+                System.out.println(naves.get(i));
+            else if (naves.get(i).contains("Numero de Identificacion")) {
+                System.out.println(naves.get(i));
+            }
+        }
+        System.out.println("Introduzca la matricula de la nave que quiera poner en venta:");
+        Scanner sc = new Scanner(System.in);
+        String s = sc.next();
+        naves = Sistema.cogerNave(naves, s);        //ya tenemos la nave que queremos
+        List<String> lecturaOfertas = new ArrayList<>();
+        br = new BufferedReader(new FileReader("userOfertas.txt"));
+        String lineas;
+        while ((lineas = br.readLine()) != null) {
+            lecturaOfertas.add(lineas);
+        }
+        min = 0;
+        max = lecturaOfertas.size();
+        boolean found=false;
+        if (Sistema.pertenece(usuarioAmeter)) {
+            if (max != 0) {
+                while (!found && max != 0) {
+                    if (usuarioAmeter.equals(lecturaOfertas.get(min))) {
+                        min=min+1;
+                        lecturaOfertas.add(min,"Numero de oferta: "+this.nIdentificacion);
+                        min=min+1;
+                        for (int i = 0; i < naves.size(); i++) {
+                            lecturaOfertas.add(min, naves.get(i));
+                            min=min+1;
+                        }
+                        lecturaOfertas.add(min, "-");
+                        found = true;
+                    } else {
+                        min = min + 1;
+                        max = max - 1;
+                    }
+                }
+            }
+            if (!found){
+                lecturaOfertas.add(usuarioAmeter);
+                lecturaOfertas.add("Numero de oferta: "+this.nIdentificacion);
+                for (int i = 0; i < naves.size(); i++) {
+                    lecturaOfertas.add(naves.get(i));
+                }
+                lecturaOfertas.add("-");
+                lecturaOfertas.add("*");
+            }
+            FileWriter fw = new FileWriter("userOfertas.txt");
+            PrintWriter escritura = new PrintWriter(fw);
+            for (int i = 0; i < lecturaOfertas.size(); i++) {
+                escritura.println(lecturaOfertas.get(i));
+            }
+            escritura.close();
+        }
+    }
 
+    public List<String> ofertaNave(int c) {
+        List<String> lista = new ArrayList<>();
+        return lista;
     }
 
     public Date fechaLimite(int c) {
-
-
+        Date fecha = new Date();
+        return fecha;
     }
 
     public int PrecioOfertaTotal(int c) {
 
+        return c;
     }
 
     public int ProteccionTotal(int c) {
 
+        return c;
     }
 
     public int crearVotacion(int c, int d) {
 
+        return c;
     }
 
     public String crearComentario(int c, int d) {
-
+        String comentario = "";
+        return comentario;
     }
 
 
-}*/
+}
