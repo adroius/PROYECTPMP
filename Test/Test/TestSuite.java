d3d3b4777ab4715744d63a66015a622866fba935
pamonugi <pamonugi@gmail.com>
2021-05-01 19:52:47 +0200

    Patata

---
+++ Test/Test/TestSuite.java
@@ -0,0 +1,9 @@
+package Test;
+
+import org.junit.runner.RunWith;
+import org.junit.platform.runner.JUnitPlatform;
+import org.junit.platform.suite.api.SelectClass;
+
+public class TestSuite {
+    @RunWith()
+}
